5101a68a65c3a618c4731f06ab3183fc6609d92c
austin <business.mocksock@gmail.com>
2022-06-19 06:03:48 -0500

    make methods to send out information

--- src/app/pizza-order/pizza-order.model.ts
+++ src/app/pizza-order/pizza-order.model.ts
@@ -1,10 +1,25 @@
-export interface PizzaOrder {
+import { Sanitizer } from "@angular/core"
+
+export class PizzaOrder {
 
     //These are the new variables to have the info transfered to
-    orderId: Number
-    size: String
-    crust: String
-    topping: String
+    private orderId: number
+    private size: string
+    private crust: string
+    private topping: string
 
+    constructor(orderId: number, size: string, crust: string, topping: string) {
+        this.orderId = orderId
+        this.size = size
+        this.crust = crust
+        this.topping = topping
+    }
 
+    getOrderId(): number {
+        return this.orderId
+    }
+
+    getOrder() {
+        return this.crust, this.size, this.topping
+    }
 }
