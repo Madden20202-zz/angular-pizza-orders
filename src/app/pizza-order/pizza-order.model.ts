e6a9432cf8dc25927d525b39f4e124b8bb03edb7
austin <business.mocksock@gmail.com>
2022-06-19 05:23:37 -0500

    set up variables in PizzaORder

---
+++ src/app/pizza-order/pizza-order.model.ts
@@ -0,0 +1,10 @@
+export interface PizzaOrder {
+
+    //These are the new variables to have the info transfered to
+    orderId: Number
+    size: String
+    crust: String
+    topping: String
+
+
+}
