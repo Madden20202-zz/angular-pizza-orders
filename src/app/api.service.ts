721d7e2a4fdc281a36defcb3668156b302b0f41d
austin <business.mocksock@gmail.com>
2022-06-23 06:00:51 -0500

    change url for the api service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,6 +9,14 @@ export class ApiService {
   constructor(private http:HttpClient) { }
 
   getPizzaOrder() {
-    return this.http.get('http://127.0.0.1:5000/orders')
+    // For whater reason the data is not coming 
+    // from the api to here
+
+    //I made a mock server on postman, which hits the 
+    // server but still shows nothing, going through 
+    // postman tutorials to figure out how to configure it all
+    // return this.http.get('http://127.0.0.1:5000/orders')
+
+    return this.http.get('https://f97e853d-a811-450e-b21e-857e14a2016d.mock.pstmn.io')
   }
 }
